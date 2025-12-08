a7f461250c97c3f3c22977be74797529368ddf2a
Mario Mario <marioramos083@gmail.com>
2025-12-07 19:51:53 -0500

    Update app.ts

--- src/app/app.ts
+++ src/app/app.ts
@@ -7,7 +7,7 @@ import { RouterOutlet } from '@angular/router';
     RouterOutlet
   ],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss' 
 })
 export class App {
   protected readonly title = signal('quien-presta-web');
